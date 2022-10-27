5779117082907de34bee8594d43adcafad729aa2
Steve Tynor <chinenual@gmail.com>
2022-10-27 16:19:23 -0400

    working

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"gopkg.in/ini.v1"
 	"log"
-	"math"
 	"os"
 	"path"
 	"path/filepath"
@@ -13,37 +12,39 @@ import (
 	"strings"
 )
 
+const outPath = "Picasa-Faces"
+const thumbWidth = "100px"
+
+var thumbCounter = -1
+
 var base = flag.String("base", "", "root directory of the source images/.picasa.ini files")
 
 var peopleMap map[string]string = map[string]string{}
 var peopleImagesMap map[string][]string = map[string][]string{}
 
+// lots of good info on the format here:
+//      https://gist.github.com/fbuchinger/1073823/9986cc61ae67afeca2f4a2f984d7b5d4a818d4f0
+
+// convert a rect64(xxxxx) has to [left, top, right, bottom] percentages
 func convertRect64(v string) (result [4]float32) {
 	// v is a 64-bit unsigned hex in a "rect64(...)" wrapper
+	// can't just convert 4 characters at a time since Picasa sometimes omits leading 0's -
+	// so parse the string as a 64bit number, then convert that
 	var u uint64
 	var err error
-	if u, err = strconv.ParseUint(v[7:11], 16, 16); err != nil {
+	v = strings.ReplaceAll(v, ")", "")
+	v = strings.ReplaceAll(v, "rect64(", "")
+	if u, err = strconv.ParseUint(v, 16, 64); err != nil {
 		log.Fatalf("Can't parse rect64: %s: %v", v, err)
 	}
-	result[0] = float32(u) / float32(math.MaxUint16)
-	if u, err = strconv.ParseUint(v[11:15], 16, 16); err != nil {
-		log.Fatalf("Can't parse rect64: %s: %v", v, err)
-	}
-	result[1] = float32(u) / float32(math.MaxUint16)
-	if u, err = strconv.ParseUint(v[15:19], 16, 16); err != nil {
-		log.Fatalf("Can't parse rect64: %s: %v", v, err)
-	}
-	result[2] = float32(u) / float32(math.MaxUint16)
-	if u, err = strconv.ParseUint(v[19:23], 16, 16); err != nil {
-		log.Fatalf("Can't parse rect64: %s: %v", v, err)
-	}
-	result[3] = float32(u) / float32(math.MaxUint16)
+
+	result[0] = float32((u>>48)&0xffff) / 65535.0
+	result[0] = float32((u>>48)&0xffff) / 65535.0
+	result[1] = float32((u>>32)&0xffff) / 65535.0
+	result[2] = float32((u>>16)&0xffff) / 65535.0
+	result[3] = float32(u&0xffff) / 65535.0
 
 	return
-}
-
-func renderPersonThumb(name string, relImagePath string, cropCoords [4]float32) {
-
 }
 
 func process(iniFile string) {
@@ -69,6 +70,10 @@ func process(iniFile string) {
 			peopleMap[cleaned] = cleaned
 			contactMap[k] = cleaned
 		}
+		// faces picasa found but we never gave a name to use this hash
+		const UNKNOWN_HASH = "ffffffffffffffff"
+		peopleMap["UNKNOWN"] = "UNKNOWN"
+		contactMap[UNKNOWN_HASH] = "UNKNOWN"
 		//log.Printf("Contacts: %v\n", s.KeysHash())
 	}
 	// loop through the sections in sorted order so that images end up in a reasonable order
@@ -79,14 +84,12 @@ func process(iniFile string) {
 		s := cfg.Section(secName)
 		if s.HasKey("faces") {
 			l := s.Key("faces").String()
-			log.Printf("L: %s\n", l)
 			list := strings.Split(l, ";")
 			for _, pair := range list {
 				v := strings.Split(pair, ",")
 				coords := convertRect64(v[0])
 				person := contactMap[v[1]]
 				relativeImagePath := path.Join(relativeDir, s.Name())
-				log.Printf("  %s -> %s %v\n", s.Name(), person, coords)
 				peopleImagesMap[person] = append(peopleImagesMap[person], relativeImagePath)
 
 				renderPersonThumb(person, relativeImagePath, coords)
@@ -96,6 +99,7 @@ func process(iniFile string) {
 }
 
 func processFiles(dir string) {
+	log.Printf("Scanning for .picasa.ini files under %s ...\n", *base)
 	var inis []string
 	e := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		if err == nil && ".picasa.ini" == info.Name() {
@@ -127,5 +131,6 @@ func main() {
 		people = append(people, k)
 	}
 	sort.Strings(people)
-	log.Printf("result: %v\n", people)
+	//log.Printf("result: %v\n", people)
+	renderIndex(people)
 }
---
+++ render.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path"
+)
+
+func createThumb(dir string, relImagePath string, cropCoords [4]float32) (thumbname string) {
+	thumbCounter++
+	thumbname = fmt.Sprintf("thumb%05d.jpg", thumbCounter)
+	thumbPath := path.Join(dir, thumbname)
+	srcPath := path.Join(*base, relImagePath)
+
+	// coords are [left, top, right, bottom]
+	// right-left = "percentage width" - similarly bottom-top
+	const LEFT = 0
+	const TOP = 1
+	const RIGHT = 2
+	const BOTTOM = 3
+	cmd := exec.Command("magick",
+		srcPath,
+		"-gravity", "NorthWest",
+		"-crop",
+		// crop FX expressions require imagemagic7:
+		fmt.Sprintf(
+			"%d%%x%d%%+%%[fx:w*%f]+%%[fx:h*%f]",
+			int64(100.0*(cropCoords[RIGHT]-cropCoords[LEFT])),
+			int64(100.0*(cropCoords[BOTTOM]-cropCoords[TOP])),
+			cropCoords[LEFT], cropCoords[TOP]),
+		"-resize", "200x200",
+		thumbPath)
+
+	//log.Printf(cmd.String())
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	if err := cmd.Start(); err != nil {
+		log.Fatalf("Failed to spawn imagemagick convert: %s: %v\n", cmd.String(), err)
+	}
+	if err := cmd.Wait(); err != nil {
+		log.Fatalf("Failed to wait for imagemagick convert: %s: %v\n", cmd.String(), err)
+	}
+	return
+}
+
+func renderPersonThumb(name string, relImagePath string, cropCoords [4]float32) {
+	var err error
+	p := path.Join(*base, outPath)
+	thumbDir := path.Join(p, "thumbs")
+	if err = os.MkdirAll(thumbDir, 0777); err != nil {
+		log.Fatalf("Could not create output folder %s: %v\n", thumbDir, err)
+	}
+	p = path.Join(p, name+".html")
+	f, err := os.OpenFile(p, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0660)
+	if err != nil {
+		log.Fatal(err)
+	}
+	thumbName := createThumb(thumbDir, relImagePath, cropCoords)
+
+	defer f.Close()
+	f.WriteString("<a href='../" + relImagePath + "'><img style='width:" + thumbWidth + ";' src='thumbs/" + thumbName + "'></img></a>\n")
+}
+
+func renderIndex(names []string) {
+	var err error
+	p := path.Join(*base, outPath)
+	p = path.Join(p, "index.html")
+	f, err := os.OpenFile(p, os.O_CREATE|os.O_WRONLY, 0660)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	for _, name := range names {
+		f.WriteString("<a href='" + name + ".html'>" + name + "</a><br/>\n")
+
+	}
+}
