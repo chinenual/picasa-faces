838fd7474d28341075501b2696db5cc7c04045ae
Steve Tynor <chinenual@gmail.com>
2022-10-28 10:39:05 -0400

    escape urls and pathnames

--- render.go
+++ render.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/url"
 	"os"
 	"os/exec"
 	"path"
@@ -59,7 +60,7 @@ func renderPersonThumb(name string, relImagePath string, cropCoords [4]float32) 
 	if err = os.MkdirAll(thumbDir, 0777); err != nil {
 		log.Fatalf("Could not create output folder %s: %v\n", thumbDir, err)
 	}
-	p = path.Join(p, name+".html")
+	p = path.Join(p, url.QueryEscape(name)+".html")
 	f, err := os.OpenFile(p, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0660)
 	if err != nil {
 		log.Fatal(err)
@@ -67,7 +68,7 @@ func renderPersonThumb(name string, relImagePath string, cropCoords [4]float32) 
 	defer f.Close()
 	thumbName := createThumb(thumbDir, relImagePath, cropCoords)
 	if thumbName != "" {
-		f.WriteString("<a href='../" + relImagePath + "'><img style='width:" + thumbWidth + ";' src='thumbs/" + thumbName + "'></img></a>\n")
+		f.WriteString("<a href='../" + url.QueryEscape(relImagePath) + "'><img style='width:" + thumbWidth + ";' src='thumbs/" + thumbName + "'></img></a>\n")
 	}
 }
 
@@ -81,7 +82,7 @@ func renderIndex(names []string) {
 	}
 	defer f.Close()
 	for _, name := range names {
-		f.WriteString("<a href='" + name + ".html'>" + name + "</a><br/>\n")
+		f.WriteString("<a href='" + url.QueryEscape(name) + ".html'>" + name + "</a><br/>\n")
 
 	}
 }
